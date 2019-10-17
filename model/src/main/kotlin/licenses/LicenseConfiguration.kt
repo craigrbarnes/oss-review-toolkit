cf32c31687a4c2dbd34b0961b78288bc5c722896
Frank Viernau <frank.viernau@here.com>
2019-10-17 13:14:46 +0200

    model: Add License configuration
    Signed-off-by: Frank Viernau <frank.viernau@here.com>

---
+++ model/src/main/kotlin/licenses/LicenseConfiguration.kt
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2017-2019 HERE Europe B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package com.here.ort.model.licenses
+
+import com.here.ort.model.yamlMapper
+
+data class LicenseConfiguration(
+    val licenseSets: List<LicenseSet> = emptyList(),
+    val licenses: List<License> = emptyList()
+) {
+    init {
+        require(licenseSets.groupBy{ it.id }.values.all { it.size == 1 } )
+        require(licenses.groupBy{ it.id }.values.all { it.size == 1 } )
+
+        // TODO: check that license class has entry in classes
+    }
+
+    fun writeAsYaml(): String = yamlMapper.writeValueAsString(this)
+}
